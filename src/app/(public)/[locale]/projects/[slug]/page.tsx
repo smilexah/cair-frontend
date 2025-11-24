e05bb1a792a30658d05e4c932223400faeb84dc8
Meiirzhan Muratbekuly <smilexah@inbox.ru>
2025-11-24 19:08:19 +0500

    Update page.tsx

--- src/app/(public)/[locale]/projects/[slug]/page.tsx
+++ src/app/(public)/[locale]/projects/[slug]/page.tsx
@@ -10,7 +10,7 @@ import type { Metadata } from "next";
 export async function generateStaticParams() {
     const projects = await getProjects();
     return projects.flatMap(project =>
-        ['en', 'ru', 'kz'].map(locale => ({
+        ['en', 'ru', 'kk'].map(locale => ({
             locale,
             slug: project.slug
         }))
